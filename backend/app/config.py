9909a60afade56fae14022835b4bf4efd7dd27df
lisaanthro <liza_vater@yahoo.com>
2024-05-16 16:29:38 +0300

    docker compose fixes

--- backend/app/config.py
+++ backend/app/config.py
@@ -9,7 +9,7 @@ class Settings(BaseSettings):
     DB_HOST: str
     DB_PORT: int
     DB_NAME: str
-    model_config = SettingsConfigDict(env_file="app/.env")
+    model_config = SettingsConfigDict(env_file="/app/.env")
 
 
 @lru_cache
